ef302930d985ede56bf9dbc3a265a173dbb5685f
MarkusSchrisch <markus_hirsch@gmx.net>
2021-04-14 12:53:14 +0200

    no message

---
+++ src/main/java/AE_Schulprojekt/AE_Schulprojekt/App.java
@@ -0,0 +1,13 @@
+package AE_Schulprojekt.AE_Schulprojekt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
