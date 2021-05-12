13af5f1533aa17a7dccd5e0965a1b6cf05c31f16
MarkusSchrisch <markus_hirsch@gmx.net>
2021-05-12 11:55:49 +0200

    neues Maven Projekt

---
+++ src/main/java/AE_Schulprojekt2/AE_Schulprojekt2/App.java
@@ -0,0 +1,13 @@
+package AE_Schulprojekt2.AE_Schulprojekt2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
